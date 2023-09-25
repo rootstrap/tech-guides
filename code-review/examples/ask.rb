7f739683d9f05ba5e91d45bf7a0b894744d55e37
Alan Lamas <alan12394@hotmail.com>
2023-09-25 00:09:44 -0300

    create example for asking

---
+++ code-review/examples/ask.rb
@@ -0,0 +1,8 @@
+
+class PeopleService
+	def process_people
+		People.connection.execute(
+			"UPDATE people SET name = CONCAT(name, ' ', surname), enabled = TRUE WHERE created_at <= '#{2.years.ago}'"
+		)
+	end 
+end
